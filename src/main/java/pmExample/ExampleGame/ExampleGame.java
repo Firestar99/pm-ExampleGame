46b0aed67befdadafc25aaa1e87f0a885859a713
Firestar99 <31222740+firestar99@users.noreply.github.com>
2019-09-24 15:45:35 +0200

    game object

--- src/main/java/pmExample/ExampleGame/ExampleGame.java
+++ src/main/java/pmExample/ExampleGame/ExampleGame.java
@@ -4,20 +4,30 @@ import static org.lwjgl.glfw.GLFW.*;
 
 public class ExampleGame {
 	
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
+		new ExampleGame().run();
+	}
+	
+	private long windowPointer;
+	
+	public void run() {
 		try {
 			glfwInit();
 			glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 			glfwWindowHint(GLFW_VERSION_MAJOR, 2);
 			glfwWindowHint(GLFW_VERSION_MINOR, 1);
 			glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_FALSE);
-			long windowPointer = glfwCreateWindow(800, 600, "Hi Window!", 0, 0);
+			windowPointer = glfwCreateWindow(800, 600, "Hi Window!", 0, 0);
 			
 			while (!glfwWindowShouldClose(windowPointer)) {
 				glfwSwapBuffers(windowPointer);
 				glfwPollEvents();
 				
-				Thread.sleep(1000 / 60);
+				try {
+					Thread.sleep(1000 / 60);
+				} catch (InterruptedException ignored) {
+				
+				}
 			}
 			
 			glfwDestroyWindow(windowPointer);
